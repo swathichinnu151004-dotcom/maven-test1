55c8cf1a7578eb9be4652fb34d44fe80eacfe598
Swathi <swathichinnu151004@gmail.com>
2026-04-16 21:42:48 +0530

    SWATHIS0001 - Added even/odd function in Utility.java

---
+++ src/test/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,12 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    // Example: Check even or odd
+    public static String checkEvenOdd(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
